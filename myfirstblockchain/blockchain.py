0c051285120b360e89f80312f6743f788c320b52
Frederico Dal Grande <dalgrande@gmail.com>
2020-07-24 21:27:55 -0300

    first commit

---
+++ myfirstblockchain/blockchain.py
@@ -0,0 +1,27 @@
+# My first lovely blockchain
+
+# Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Building a Blockchain
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, previous_hash="0")
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": str(datetime.datetime.now()),
+            "proof": proof,
+            "previous_hash": previous_hash,
+        }
+        self.chain.append(block)
+        return block
+
+
+# Mining the Blockchain
+
